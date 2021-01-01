154dfb60d6c1260875d9dd4d2e6704dcdf73f578
Jerzy <info@nanet.uk>
2021-01-01 13:52:59 +0000

    old files

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+define('DOMAIN_NAME', $_SERVER['SERVER_NAME']);
+?><!doctype html>
+<html lang="en">
+  <head>
+    <?php include 'theme/header.php'; ?>
+  </head>
+  <body>
+    <h1>Domain for sale! <?php echo DOMAIN_NAME; ?></h1>
+
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+  </body>
+</html>
