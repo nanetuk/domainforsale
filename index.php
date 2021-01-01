f7ac83bff396292bebfedc6e587d8c90fbcd3282
Jerzy <info@nanet.uk>
2021-01-01 14:39:32 +0000

    form elements

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ define('DOMAIN_NAME', $_SERVER['SERVER_NAME']);
                 <label for="firstName" class="form-label">Name</label>
                 <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
                 <div class="invalid-feedback">
-                  Valid first name is required.
+                  Valid name is required.
                 </div>
               </div>
 
@@ -36,64 +36,17 @@ define('DOMAIN_NAME', $_SERVER['SERVER_NAME']);
                 <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
                 <input type="email" class="form-control" id="email" placeholder="you@example.com">
                 <div class="invalid-feedback">
-                  Please enter a valid email address for shipping updates.
+                  Please enter a valid email address.
                 </div>
               </div>
 
               <div class="col-12">
-                <label for="address" class="form-label">Address</label>
+                <label for="address" class="form-label">MAx Bid</label>
                 <input type="text" class="form-control" id="address" placeholder="1234 Main St" required>
                 <div class="invalid-feedback">
-                  Please enter your shipping address.
+                  Please enter your max bid.
                 </div>
               </div>
-
-              <div class="col-12">
-                <label for="address2" class="form-label">Address 2 <span class="text-muted">(Optional)</span></label>
-                <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
-              </div>
-
-              <div class="col-md-5">
-                <label for="country" class="form-label">Country</label>
-                <select class="form-select" id="country" required>
-                  <option value="">Choose...</option>
-                  <option>United States</option>
-                </select>
-                <div class="invalid-feedback">
-                  Please select a valid country.
-                </div>
-              </div>
-
-              <div class="col-md-4">
-                <label for="state" class="form-label">State</label>
-                <select class="form-select" id="state" required>
-                  <option value="">Choose...</option>
-                  <option>California</option>
-                </select>
-                <div class="invalid-feedback">
-                  Please provide a valid state.
-                </div>
-              </div>
-
-              <div class="col-md-3">
-                <label for="zip" class="form-label">Zip</label>
-                <input type="text" class="form-control" id="zip" placeholder="" required>
-                <div class="invalid-feedback">
-                  Zip code required.
-                </div>
-              </div>
-            </div>
-
-            <hr class="my-4">
-
-            <div class="form-check">
-              <input type="checkbox" class="form-check-input" id="same-address">
-              <label class="form-check-label" for="same-address">Shipping address is the same as my billing address</label>
-            </div>
-
-            <div class="form-check">
-              <input type="checkbox" class="form-check-input" id="save-info">
-              <label class="form-check-label" for="save-info">Save this information for next time</label>
             </div>
 
             <hr class="my-4">
