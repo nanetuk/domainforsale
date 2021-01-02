488feee7ad1256a7ff3dfcc5af3005ee2429e1d2
Jerzy <info@nanet.uk>
2021-01-02 13:54:15 +0000

    add js to index

--- theme/footer.php
+++ theme/footer.php
@@ -5,3 +5,12 @@
     <li class="list-inline-item"><a href="https://phpsql.org">phpsql.org</a></li>
   </ul>
 </footer>
+
+<!-- Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+<!-- Form validation -->
+<script src="js/validation.js"></script>
+
+<!-- Parallax background effect -->
+<script src="js/parallax.js"></script>
