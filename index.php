9453182f980e67e77c37b220df1acda921d08488
Jerzy <info@nanet.uk>
2021-01-02 13:42:33 +0000

    parallax id

--- index.php
+++ index.php
@@ -52,65 +52,66 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && valideteForm() === true) {
     <?php include 'theme/header.php'; ?>
   </head>
   <body>
-    <main class="container">
-      <div class="py-5 text-center">
-        <i class="fas fa-pound-sign"></i>
-        <h2><?php echo DOMAIN_NAME; ?> is for sale!</h2>
-        <p class="lead">DOMAIN NAME VALUES ARE CURRENTLY RISING AT A VERY RAPID RATE!</p>
-        <?php if ($emailSent) { ?>
-          <div class="alert alert-success">Email has been sent. We will come back to you soon!</div><?php } ?>
-        <?php if ($emailError) { ?>
-          <div class="alert alert-warning">Errors while sending your email:<br><?php echo $emailError; ?></div><?php } ?>
-      </div>
-
-      <div class="row g-3">
-        <div class="col-md-7 col-lg-8">
-          <h4 class="mb-3">Domain statistics</h4>
+    <div id="parallax">
+      <main class="container">
+        <div class="py-5 text-center">
+          <i class="fas fa-pound-sign"></i>
+          <h2><?php echo DOMAIN_NAME; ?> is for sale!</h2>
+          <p class="lead">DOMAIN NAME VALUES ARE CURRENTLY RISING AT A VERY RAPID RATE!</p>
+          <?php if ($emailSent) { ?>
+            <div class="alert alert-success">Email has been sent. We will come back to you soon!</div><?php } ?>
+          <?php if ($emailError) { ?>
+            <div class="alert alert-warning">Errors while sending your email:<br><?php echo $emailError; ?></div><?php } ?>
         </div>
 
-        <div class="col-md-5 col-lg-4">
-          <h4 class="d-flex justify-content-between align-items-center mb-3">
-            <span class="text-muted">MAKE YOUR OFFER</span>
-          </h4>
-          <form class="needs-validation" method="POST" novalidate>
-            <div class="row g-3">
-              <div class="col-12">
-                <label for="name" class="form-label">Name</label>
-                <input name="name" type="text" class="form-control" id="name" placeholder="eg. George" value="" required>
-                <div class="invalid-feedback">
-                  Valid name is required.
+        <div class="row g-3">
+          <div class="col-md-7 col-lg-8">
+            <h4 class="mb-3">Domain statistics</h4>
+          </div>
+
+          <div class="col-md-5 col-lg-4">
+            <h4 class="d-flex justify-content-between align-items-center mb-3">
+              <span class="text-muted">MAKE YOUR OFFER</span>
+            </h4>
+            <form class="needs-validation" method="POST" novalidate>
+              <div class="row g-3">
+                <div class="col-12">
+                  <label for="name" class="form-label">Name</label>
+                  <input name="name" type="text" class="form-control" id="name" placeholder="eg. George" value="" required>
+                  <div class="invalid-feedback">
+                    Valid name is required.
+                  </div>
                 </div>
+
+                <div class="col-12">
+                  <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
+                  <input name="email" type="email" class="form-control" id="email" placeholder="eg. you@example.com" required>
+                  <div class="invalid-feedback">
+                    Please enter a valid email address.
+                  </div>
+                </div>
+
+                <div class="col-12">
+                  <label for="address" class="form-label">Max bid</label>
+                  <div class="input-group">
+                    <span class="input-group-text">&pound;</span>
+                      <input name="bid" type="text" class="form-control" id="bid" placeholder="eg. 400" required="">
+                      <div class="invalid-feedback">
+                          Your bid is required.
+                      </div>
+                  </div>
+                </div>
+
               </div>
 
-              <div class="col-12">
-                <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
-                <input name="email" type="email" class="form-control" id="email" placeholder="eg. you@example.com" required>
-                <div class="invalid-feedback">
-                  Please enter a valid email address.
-                </div>
-              </div>
+              <hr class="my-4">
 
-              <div class="col-12">
-                <label for="address" class="form-label">Max bid</label>
-                <div class="input-group">
-                  <span class="input-group-text">&pound;</span>
-                    <input name="bid" type="text" class="form-control" id="bid" placeholder="eg. 400" required="">
-                    <div class="invalid-feedback">
-                        Your bid is required.
-                    </div>
-                </div>
-              </div>
-
-            </div>
-
-            <hr class="my-4">
-
-            <button class="w-100 btn btn-primary btn-lg" type="submit">Submit</button>
-          </form>
+              <button class="w-100 btn btn-primary btn-lg" type="submit">Submit</button>
+            </form>
+          </div>
         </div>
-      </div>
-    </main>
-
+      </main>
+    </div>
     <?php include 'theme/footer.php'; ?>
 
     <!-- Bootstrap Bundle with Popper -->
