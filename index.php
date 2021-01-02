d7736a7334bb9f5fd6007fd778b3ba87271e775f
Jerzy <info@nanet.uk>
2021-01-02 14:08:12 +0000

    remofe double form

--- index.php
+++ index.php
@@ -53,44 +53,42 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && valideteForm() === true) {
   </head>
   <body class="text-center parallax">
     <main class="form-signin">
-      <form>
       <i class="fas fa-pound-sign mb-4"></i>
-        <h1 class="h3 mb-3 fw-normal"><?php echo DOMAIN_NAME; ?> is for sale!</h1>
-        <p class="lead">DOMAIN NAME VALUES ARE CURRENTLY RISING AT A VERY RAPID RATE!</p>
-        <?php if ($emailSent) { ?><div class="alert alert-success">Email has been sent. We will come back to you soon!</div><?php } ?>
-        <?php if ($emailError) { ?><div class="alert alert-warning">Errors while sending your email:<br><?php echo $emailError; ?></div><?php } ?>
-        <form class="needs-validation" method="POST" novalidate>
-          <div class="row g-3">
-            <div class="col-12">
-              <label for="name" class="form-label">Name</label>
-              <input name="name" type="text" class="form-control" id="name" placeholder="eg. George" value="" required>
-              <div class="invalid-feedback">
-                Valid name is required.
-              </div>
-            </div>
-
-            <div class="col-12">
-              <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
-              <input name="email" type="email" class="form-control" id="email" placeholder="eg. you@example.com" required>
-              <div class="invalid-feedback">
-                Please enter a valid email address.
-              </div>
-            </div>
-
-            <div class="col-12">
-              <label for="address" class="form-label">Max bid</label>
-              <div class="input-group">
-                <span class="input-group-text">&pound;</span>
-                  <input name="bid" type="text" class="form-control" id="bid" placeholder="eg. 400" required="">
-                  <div class="invalid-feedback">
-                    Your bid is required.
-                  </div>
-              </div>
+      <h1 class="h3 mb-3 fw-normal"><?php echo DOMAIN_NAME; ?> is for sale!</h1>
+      <p class="lead">DOMAIN NAME VALUES ARE CURRENTLY RISING AT A VERY RAPID RATE!</p>
+      <?php if ($emailSent) { ?><div class="alert alert-success">Email has been sent. We will come back to you soon!</div><?php } ?>
+      <?php if ($emailError) { ?><div class="alert alert-warning">Errors while sending your email:<br><?php echo $emailError; ?></div><?php } ?>
+      <form class="needs-validation" method="POST" novalidate>
+        <div class="row g-3">
+          <div class="col-12">
+            <label for="name" class="form-label">Name</label>
+            <input name="name" type="text" class="form-control" id="name" placeholder="eg. George" value="" required>
+            <div class="invalid-feedback">
+              Valid name is required.
             </div>
           </div>
 
-          <button class="w-100 btn btn-primary btn-lg" type="submit">Submit</button>
-        </form>
+          <div class="col-12">
+            <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
+            <input name="email" type="email" class="form-control" id="email" placeholder="eg. you@example.com" required>
+            <div class="invalid-feedback">
+              Please enter a valid email address.
+            </div>
+          </div>
+
+          <div class="col-12">
+            <label for="address" class="form-label">Max bid</label>
+            <div class="input-group">
+              <span class="input-group-text">&pound;</span>
+                <input name="bid" type="text" class="form-control" id="bid" placeholder="eg. 400" required="">
+                <div class="invalid-feedback">
+                  Your bid is required.
+                </div>
+            </div>
+          </div>
+        </div>
+
+        <button class="w-100 btn btn-primary btn-lg" type="submit">Submit</button>
       </form>
     </main>
     <?php include 'theme/footer.php'; ?>
