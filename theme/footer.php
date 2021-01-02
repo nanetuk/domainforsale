c0a087f7601003ebff812ad2d2f2d8685dacd507
Jerzy <info@nanet.uk>
2021-01-02 14:03:04 +0000

    new design

--- theme/footer.php
+++ theme/footer.php
@@ -1,11 +1,3 @@
-<footer class="my-5 pt-5 text-muted text-center text-small">
-  <p class="mb-1">&copy; <?php echo date('Y'); ?> Copyright <?php echo DOMAIN_NAME; ?></p>
-  <ul class="list-inline">
-    <li class="list-inline-item"><a href="https://evaart.co.uk">evaart.co.uk</a></li>
-    <li class="list-inline-item"><a href="https://phpsql.org">phpsql.org</a></li>
-  </ul>
-</footer>
-
 <!-- Bootstrap Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
